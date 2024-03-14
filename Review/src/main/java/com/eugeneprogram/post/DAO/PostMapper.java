6f17b1600db33b4475b279572ea08c0515eca869
minhe <minhe@DESKTOP-23AQU4U>
2024-03-14 18:17:44 +0900

    test

--- Review/src/main/java/com/eugeneprogram/post/DAO/PostMapper.java
+++ Review/src/main/java/com/eugeneprogram/post/DAO/PostMapper.java
@@ -3,7 +3,7 @@ package com.eugeneprogram.post.DAO;
 import java.util.List;
 import java.util.Map;
 
-// by feature_branch aaaas
+// by feature_branch abcd
 
 public interface PostMapper {
 	public List<Map<String, Object>> getList() throws Exception;
