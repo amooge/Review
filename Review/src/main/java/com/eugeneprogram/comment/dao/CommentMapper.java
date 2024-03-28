3902f5da6436bf9e5fbd7a495bdaddf9005be95d
Ldhzm <160833192+Ldhzm@users.noreply.github.com>
2024-03-28 15:18:20 +0900

    Update CommentMapper.java

--- Review/src/main/java/com/eugeneprogram/comment/dao/CommentMapper.java
+++ Review/src/main/java/com/eugeneprogram/comment/dao/CommentMapper.java
@@ -1,5 +1,17 @@
 package com.eugeneprogram.comment.dao;
 
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface CommentMapper {
 
+	public List<Map<String, Object>> commentList() throws Exception;
+	public Map<String, Object> getCommentById(long cmtId) throws Exception;
+	public void insertComment(Map<String, Object>cmt) throws Exception;
+	public void updateComment(Map<String, Object>cmt) throws Exception;
+	public void deleteComment(long cmtId) throws Exception;
 }
