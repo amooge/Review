14c70fd1473f92c253a1bde23169efd37fdcc9d2
Ldhzm <160833192+Ldhzm@users.noreply.github.com>
2024-03-28 15:19:02 +0900

    Update CommentService.java

--- Review/src/main/java/com/eugeneprogram/comment/service/CommentService.java
+++ Review/src/main/java/com/eugeneprogram/comment/service/CommentService.java
@@ -1,5 +1,51 @@
-package com.eugeneprogram.comment.service;
+ackage com.eugeneprogram.comment.service;
 
+import java.util.List;
+import java.util.Map;
+//import java.time.LocalDateTime;
+//import java.time.LocalDateTime;
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eugeneprogram.comment.dao.CommentMapper;
+
+@Service
 public class CommentService {
 
+	@Autowired
+    CommentMapper commentMapper;
+
+    public List<Map<String, Object>> commentList() throws Exception {
+        
+    	return commentMapper.commentList();
+    }
+
+    public Map<String, Object> getComment(Long cmtId)throws Exception{
+		Map<String, Object> comment = new HashMap<String, Object>();
+		//Long 0 == null
+		if(cmtId.equals(null)) {
+			comment.put("text", "");
+			
+		}else {
+		 comment = commentMapper.getCommentById(cmtId);
+		 
+		}
+		return comment;
+	}
+    //추가 업데이트
+    public void addAndUpdate(Map<String, Object> cmt) throws Exception{
+		if(commentMapper.getCommentById((long) cmt.get("cmtId")) == null || cmt.get("cmtId") == null) {
+			//cmt.put("cmtCreateDate", LocalDateTime.now());
+			commentMapper.insertComment(cmt);
+		}else {
+			//cmt.put("pstUpdateDate", LocalDateTime.now());
+			commentMapper.updateComment(cmt);
+		}
+	}  
+    //삭제
+    public void deleteComment(long cmtId) throws Exception{
+		commentMapper.deleteComment(cmtId);
+	}
 }
